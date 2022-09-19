b9715e91811df52f218065667e95d2fac7f01e3e
Matias Bertuccio <tutebertuccio@gmail.com>
2022-09-19 11:12:30 -0300

    Agrega manejarJuego + obtenerCuadradoRandom + iluminarCuadrado
    manejarJuego es agregado para ser el esqueleto del juego, mientras que obtenerCuadradoRandom es creado para generar un cuadrado aleatorio e iluminarCuadrado nos sirve para demarcar al cuadrado aleatorio

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ document.querySelector('#boton-empezar').onclick = empezarJuego;
 function empezarJuego() {
     reiniciarSecuencias();
     actualizarEstado('¡Jugando!');
+    manejarJuego();
 }
 
 function reiniciarSecuencias() {
@@ -17,4 +18,25 @@ function reiniciarSecuencias() {
 
 function actualizarEstado(estado) {
     document.querySelector('#estado').textContent = estado;
+}
+
+function manejarJuego() {
+    const cuadradoRandom = obtenerCuadradoRandom();
+    secuenciaMaquina.push(cuadradoRandom);
+    iluminarCuadrado(cuadradoRandom);
+}
+
+function obtenerCuadradoRandom() {
+    const $cuadrados = Number(document.querySelectorAll('.cuadrado').length);
+    const numeroRandom = Math.floor(Math.random() * $cuadrados);
+    
+    return document.querySelectorAll('.cuadrado')[numeroRandom];
+}
+
+function iluminarCuadrado(cuadradoRandom) {
+    cuadradoRandom.style.opacity = 1;
+
+    setTimeout(function() {
+        cuadradoRandom.style.opacity = 0.5;
+    }, 500);
 }
